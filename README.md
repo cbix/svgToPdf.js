d21b5f94df53915af8c8ba7ebf833a40328dab8d
Florian Hülsmann <fh@cbix.de>
2012-04-17 01:46:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# What?
+Hey, if your imagination can't extract this information out of the project's title, you may not be able to use it... But yes, it's a pure javascript converter for SVG to PDF.
+
+# Seriously?
+Yes! I started this for making PDF export from [svg-edit](http://code.google.com/p/svg-edit) possible.
+
+# Capabilities
+It can convert text, lines, rectangles, ellipses and groups of these elements.
+
+# Usage
+    // I recommend to keep the svg visible as a preview
+    var svg = $('#container > svg').get(0);
+	// you should set the format dynamically, write [width, height] instead of 'a4'
+	var pdf = new jsPDF('p', 'pt', 'a4');
+	svgElementToPdf(svg, pdf, {
+		scale: 72/96, // this is the ratio of px to pt units
+		removeInvalid: true // this removes elements that could not be translated to pdf from the source svg
+	});
+	pdf.output('datauri'); // use output() to get the jsPDF buffer
+
+# Requirements:
+* [jsPDF](https://github.com/MrRio/jsPDF)
+* [jQuery](http://jquery.org)
---
+++ svgToPdf.js
@@ -0,0 +1,208 @@
+/*
+ * svgToPdf.js
+ * 
+ * Copyright 2012 Florian Hülsmann <fh@cbix.de>
+ * 
+ * This script is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+var pdfSvgAttr = {
+    // allowed attributes. all others are removed from the preview.
+    g: ['stroke', 'fill', 'stroke-width'],
+    line: ['x1', 'y1', 'x2', 'y2', 'stroke', 'stroke-width'],
+    rect: ['x', 'y', 'width', 'height', 'stroke', 'fill', 'stroke-width'],
+    ellipse: ['cx', 'cy', 'rx', 'ry', 'stroke', 'fill', 'stroke-width'],
+    circle: ['cx', 'cy', 'r', 'stroke', 'fill', 'stroke-width'],
+    text: ['x', 'y', 'font-size', 'font-family', 'text-anchor', 'font-weight', 'font-style', 'fill']
+};
+var svgElementToPdf = function(element, pdf, options) {
+    // pdf is a jsPDF object
+    //console.log("options =", options);
+    var remove = (typeof(options.removeInvalid) == 'undefined' ? remove = false : options.removeInvalid);
+    var k = (typeof(options.removeInvalid) == 'undefined' ? 1.0 : options.scale);
+    var colorMode = null;
+    $(element).children().each(function(i, node) {
+        //console.log("passing: ", node);
+        var n = $(node);
+		var hasFillColor = false;
+		var hasStrokeColor = false;
+		if(n.is('g,line,rect,ellipse,circle,text')) {
+            var fillColor = n.attr('fill');
+            if(typeof(fillColor) != 'undefined') {
+                var fillRGB = new RGBColor(fillColor);
+                if(fillRGB.ok) {
+					hasFillColor = true;
+                    colorMode = 'F';
+                } else {
+                    colorMode = null;
+                }
+            }
+        }
+        if(n.is('g,line,rect,ellipse,circle')) {
+            if(hasFillColor) {
+				pdf.setFillColor(fillRGB.r, fillRGB.g, fillRGB.b);
+			}
+            if(typeof(n.attr('stroke-width')) != 'undefined') {
+                pdf.setLineWidth(k * parseInt(n.attr('stroke-width')));
+            }
+            var strokeColor = n.attr('stroke');
+            if(typeof(strokeColor) != 'undefined') {
+                var strokeRGB = new RGBColor(strokeColor);
+                if(strokeRGB.ok) {
+					hasStrokeColor = true;
+                    pdf.setDrawColor(strokeRGB.r, strokeRGB.g, strokeRGB.b);
+                    if(colorMode == 'F') {
+                        colorMode = 'FD';
+                    } else {
+                        colorMode = null;
+                    }
+                } else {
+                    colorMode = null;
+                }
+            }
+		}
+        switch(n.get(0).tagName.toLowerCase()) {
+            case 'svg':
+            case 'a':
+            case 'g':
+                svgElementToPdf(node, pdf, options);
+                $.each(node.attributes, function(i, a) {
+                    if(typeof(a) != 'undefined' && pdfSvgAttr.g.indexOf(a.name.toLowerCase()) == -1) {
+                        node.removeAttribute(a.name);
+                    }
+                });
+                break;
+            case 'line':
+                pdf.line(
+                    k*parseInt(n.attr('x1')),
+                    k*parseInt(n.attr('y1')),
+                    k*parseInt(n.attr('x2')),
+                    k*parseInt(n.attr('y2'))
+                );
+                $.each(node.attributes, function(i, a) {
+                    if(typeof(a) != 'undefined' && pdfSvgAttr.line.indexOf(a.name.toLowerCase()) == -1) {
+                        node.removeAttribute(a.name);
+                    }
+                });
+                break;
+            case 'rect':
+                pdf.rect(
+                    k*parseInt(n.attr('x')),
+                    k*parseInt(n.attr('y')),
+                    k*parseInt(n.attr('width')),
+                    k*parseInt(n.attr('height')),
+                    colorMode
+                );
+                $.each(node.attributes, function(i, a) {
+                    if(typeof(a) != 'undefined' && pdfSvgAttr.rect.indexOf(a.name.toLowerCase()) == -1) {
+                        node.removeAttribute(a.name);
+                    }
+                });
+                break;
+            case 'ellipse':
+                pdf.ellipse(
+                    k*parseInt(n.attr('cx')),
+                    k*parseInt(n.attr('cy')),
+                    k*parseInt(n.attr('rx')),
+                    k*parseInt(n.attr('ry')),
+                    colorMode
+                );
+                $.each(node.attributes, function(i, a) {
+                    if(typeof(a) != 'undefined' && pdfSvgAttr.ellipse.indexOf(a.name.toLowerCase()) == -1) {
+                        node.removeAttribute(a.name);
+                    }
+                });
+                break;
+            case 'circle':
+                pdf.circle(
+                    k*parseInt(n.attr('cx')),
+                    k*parseInt(n.attr('cy')),
+                    k*parseInt(n.attr('r')),
+                    colorMode
+                );
+                $.each(node.attributes, function(i, a) {
+                    if(typeof(a) != 'undefined' && pdfSvgAttr.circle.indexOf(a.name.toLowerCase()) == -1) {
+                        node.removeAttribute(a.name);
+                    }
+                });
+                break;
+            case 'text':
+                if(node.hasAttribute('font-family')) {
+                    switch(n.attr('font-family').toLowerCase()) {
+                        case 'serif': pdf.setFont('times'); break;
+                        case 'monospace': pdf.setFont('courier'); break;
+                        default:
+                            n.attr('font-family', 'sans-serif');
+                            pdf.setFont('helvetica');
+                    }
+                }
+                if(hasFillColor) {
+                    pdf.setTextColor(fillRGB.r, fillRGB.g, fillRGB.b);
+                }
+                var fontType = "";
+                if(node.hasAttribute('font-weight')) {
+                    if(n.attr('font-weight') == "bold") {
+                        fontType = "bold";
+                    } else {
+                        node.removeAttribute('font-weight');
+                    }
+                }
+                if(node.hasAttribute('font-style')) {
+                    if(n.attr('font-style') == "italic") {
+                        fontType += "italic";
+                    } else {
+                        node.removeAttribute('font-style');
+                    }
+                }
+                pdf.setFontType(fontType);
+                var pdfFontSize = 16;
+				if(node.hasAttribute('font-size')) {
+                    pdfFontSize = parseInt(n.attr('font-size'));
+                }
+                var box = node.getBBox();
+                //FIXME: use more accurate positioning!!
+                var x, y, xOffset = 0;
+                if(node.hasAttribute('text-anchor')) {
+                    switch(n.attr('text-anchor')) {
+                        case 'end': xOffset = box.width; break;
+                        case 'middle': xOffset = box.width / 2; break;
+                        case 'start': break;
+                        case 'default': n.attr('text-anchor', 'start');
+                    }
+                    x = parseInt(n.attr('x')) - xOffset;
+                    y = parseInt(n.attr('y'));
+                }
+				//console.log("fontSize:", pdfFontSize, "text:", n.text());
+                pdf.setFontSize(pdfFontSize).text(
+                    k * x,
+                    k * y,
+                    n.text()
+                );
+                $.each(node.attributes, function(i, a) {
+                    if(typeof(a) != 'undefined' && pdfSvgAttr.text.indexOf(a.name.toLowerCase()) == -1) {
+                        node.removeAttribute(a.name);
+                    }
+                });
+                break;
+            //TODO: image
+            default:
+                if(remove) {
+                    console.log("can't translate to pdf:", node);
+                    n.remove();
+                }
+        }
+    });
+    return pdf;
+}
